92158a0769f3c63b7ad751630eded69ba0398a7e
meth <mth@mthweb.org>
2012-12-17 18:58:25 +0100

    Some updates, added custom exceptions

--- YExtend/Controller/Abstract.php
+++ YExtend/Controller/Abstract.php
@@ -8,7 +8,94 @@ class Exception_404 extends Exception
 {
     public function __construct( YExtend_Controller_Abstract $e )
     {
-        $e->getResponse()->setRedirect( '/not-found' );
-        Yaf_Dispatcher::getInstance()->disableView();
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 404 Not Found' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
+    }
+}
+
+class Exception_403 extends Exception
+{
+    public function __construct( YExtend_Controller_Abstract $e )
+    {
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 403 Forbidden' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
+    }
+}
+
+class Exception_500 extends Exception
+{
+    public function __construct( YExtend_Controller_Abstract $e )
+    {
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 500 Internal Server Error' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
+    }
+}
+
+class Exception_415 extends Exception
+{
+    public function __construct( YExtend_Controller_Abstract $e )
+    {
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 415 Unsupported Media Type' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
+    }
+}
+
+class Exception_409 extends Exception
+{
+    public function __construct( YExtend_Controller_Abstract $e )
+    {
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 409 Conflict' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
+    }
+}
+
+class Exception_417 extends Exception
+{
+    public function __construct( YExtend_Controller_Abstract $e )
+    {
+        if ( $e instanceof YExtend_Controller_Ajax )
+        {
+            header( 'HTTP/1.0 417 Expectation Failed' );
+        }
+        else
+        {
+            $e->getResponse()->setRedirect( '/not-found' );
+            Yaf_Dispatcher::getInstance()->disableView();
+        }
     }
 }
---
+++ YExtend/Exception.php
@@ -0,0 +1,9 @@
+<?php
+
+class YExtend_Exception extends Exception
+{
+    public function __construct()
+    {
+
+    }
+}
